74388d9b38ccd2a121b1363a15abe6a5f1d195c0
Dimitrios C. Michalakos <dimitris@jmike.gr>
2015-09-17 11:44:36 +0300

    Unit testing

--- test/customerror.js
+++ test/customerror.js
@@ -8,13 +8,28 @@ describe('CustomError', function() {
       assert.throws(() => new CustomError(123));
       assert.throws(() => new CustomError(true));
       assert.throws(() => new CustomError(null));
+      assert.throws(() => new CustomError({}));
       assert.throws(() => new CustomError(new Date()));
     });
+
     it('accepts string as err', function() {
-      assert.doesNotThrow(() => new CustomError('Error'));
+      assert.doesNotThrow(() => new CustomError('err'));
     });
+
     it('accepts Error instance as err', function() {
-      assert.doesNotThrow(() => new CustomError(new Error('Error')));
+      assert.doesNotThrow(() => new CustomError(new Error('err')));
+    });
+
+    it('throws error when name is invalid', function() {
+      assert.throws(() => new CustomError('err', 123));
+      assert.throws(() => new CustomError('err', true));
+      assert.throws(() => new CustomError('err', null));
+      assert.throws(() => new CustomError('err', {}));
+      assert.throws(() => new CustomError('err', new Date()));
+    });
+
+    it('accepts string as name', function() {
+      assert.doesNotThrow(() => new CustomError('err', 'SomeName'));
     });
   });
 });
